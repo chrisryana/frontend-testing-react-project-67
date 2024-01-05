d374cf4ed336ef770c477878665d0c6a7fdcf8bd
Chris Ryana <kissunchic@yandex.ru>
2024-01-05 17:12:26 +0300

    Добавлен тест на скачивание файла html

--- __tests__/pageLoader.test.js
+++ __tests__/pageLoader.test.js
@@ -1,22 +1,51 @@
-import { jest } from '@jest/globals';
 import { fileURLToPath } from 'url';
+import os from 'os';
 import fs from 'fs/promises';
 import path from 'path';
+import nock from 'nock';
 
-const _filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(_filename);
+import pageLoader, { getFilenameByUrl } from '../src/pageLoader.js';
 
-const getFixturePath = (filename) => path.join(_dirname, '..', '__fixtures__', filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const HOST = 'https://ru.hexlet.io';
+let tmpDir = '';
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
 
-beforeAll(() => {
-  
-})
+describe('page-loader', () => {
+  beforeAll(() => {
+    nock.disableNetConnect();
+  });
 
-beforeEach(() => {
-  
-})
+  afterAll(() => {
+    nock.enableNetConnect();
+  });
 
-test('тестик', () => {
-  expect(1).toBe(1)
-})
+  beforeEach(async () => {
+    tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+  });
+
+  test('getFilenameByUrl', () => {
+    const filename = getFilenameByUrl('https://ru.hexlet.io/courSES_12');
+
+    expect(filename).toBe('ru-hexlet-io-courses-12.html');
+  });
+
+  test('Скачивание html', async () => {
+    const expectHtml = await readFile('index.html');
+
+    const scope = nock(HOST)
+      .get('/courses')
+      .reply(200, expectHtml);
+
+    const expectFilepath = path.join(tmpDir, 'ru-hexlet-io-courses.html');
+    const response = await pageLoader(`${HOST}/courses`, tmpDir);
+    const resultHTML = await fs.readFile(expectFilepath, 'utf-8');
+
+    expect(scope.isDone()).toBeTruthy();
+    expect(response?.filepath).toBe(expectFilepath);
+    expect(resultHTML).toBe(expectHtml);
+  });
+});
